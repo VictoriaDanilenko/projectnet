cc2a5743b51b1543e8ac94c4cd5e5123af16531b
Victoria Danilenko <vifrontender.com>
2021-12-27 20:28:18 +0400

    change components

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# projectnet
+projectnet
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+  return <div className="content">
+    <div>
+      <img src="https://images.unsplash.
+com/photo-1455540904194-fc101941273a?
+ixlib=rb-1.2.1&
+ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZW5n
+bGlzaCUyMHN0dWR5fGVufDB8fDB8fA%3D%3D&
+auto=format&fit=crop&w=400&q=60" alt="learn english"/>
+    </div>
+    <div>ava+description</div>
+    <div>
+      My post
+      <div>New post</div>
+      <div>
+        <div>post 1</div>
+        <div>post 2</div>
+        <div>post 3</div>
+        <div>post 4</div>
+      </div>
+    </div>
+  </div>
+
+}
+
+export default Profile;
