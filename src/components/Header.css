9cf6d1800bf25ae98a48530331b09d6190a0fcac
Victoria Danilenko <vifrontender.com>
2022-01-12 18:51:23 +0400

    add css

---
+++ src/components/Header.css
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return <header className="header">
+  <img
+    src="https://cdn3.vectorstock.com/i/
+thumb-large/65/92/
+logo-or-icon-with-big-ben-for-learn-e
+nglish-vector-36666592.jpg"
+    alt="speak english"
+  />
+</header>
+}
+
+export default Header;
