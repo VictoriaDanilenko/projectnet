ad57963da779e3f1e5b99835ca720335cdab5dea
Victoria Danilenko <vifrontender.com>
2022-02-16 22:13:03 +0400

    add props

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,22 +3,23 @@ import p from './MyPosts.module.css'
 import Post from './Posts/Post'
 
 
-const MyPosts = () => {
+const MyPosts = (props) => {
+  
   return (
   <div className={p.item}>
       My posts
-      <div>
+      <div> 
         <textarea></textarea>
         <button>Add post</button>
         <button>Remove</button>
       </div>
       <div className={p.posts}>  
-        <Post message='This is my first post!'/>
-        <Post message='Hello everyone!'/>  
-        <Post message='I am happy, and you?!'/>
-        <Post message='Wow it is so cool!!'/>
-        <Post message='I like it!'/>
-        <Post message='Super communication!'/>
+        <Post message='This is my first post!' Post like='5'  />
+        <Post message='Hello everyone!' Post like='16'/>  
+        <Post message='I am happy, and you?!' Post like='7'/>
+        <Post message='Wow it is so cool!!' Post like='3'/> 
+        <Post message='I like it!' Post like='5'/>
+        <Post message='Super communication!' Post like='55'/>
       </div>
     </div>
   )
--- src/components/Profile/MyPosts/Posts/Post.jsx
+++ src/components/Profile/MyPosts/Posts/Post.jsx
@@ -6,12 +6,14 @@ const Post = (props) => {
 
   return (
         <div className={f.item}>
-          <img src="https://klike.net/uploads/posts/2019-03/1551511784_4.jpg"/>
-      {props.message}
-      <div>
-        <span>like</span>
-    </div>
-    </div>
+            <img src="https://klike.net/uploads/posts/2019-03/1551511784_4.jpg"/>
+          {props.message}
+            <div>
+        <span>like {props.like}</span>
+            </div>
+        </div>
+
+
   )
 }
 
